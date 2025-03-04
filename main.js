6c201a179be62ff4e5142a880ba48c09136492e1
Hantak226 <156984093+Hantak226@users.noreply.github.com>
2025-03-04 19:29:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Frontend Developer",
+    "Python Developer",
+    "Web Developer",
+    "UI & UX",
+  ],
+  typeSpeed: 10,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
